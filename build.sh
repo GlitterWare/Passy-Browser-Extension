014b43d1cca7df42e3cb08f763998e45b46de381
GleammerRay <101527589+GleammerRay@users.noreply.github.com>
2023-12-05 13:41:15 +0000

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+cd $(dirname 0)
+export PATH="$PATH:$PWD/submodules/flutter/bin"
+./submodules/flutter/bin/flutter build web --web-renderer html --csp
+rm -rf build/firefox
+cp -r build/web build/firefox
+rm build/firefox/manifest.json
+mv build/firefox/manifest_firefox.json build/firefox/manifest.json
+echo 'All done.'
+echo 'Chromium: $PWD/build/web'
+echo 'Firefox: $PWD/build/firefox'
