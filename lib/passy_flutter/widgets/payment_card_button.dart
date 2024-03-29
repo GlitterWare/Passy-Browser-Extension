fb8e4493631370baade4721fae195a1beaa760c9
GleammerRay <101527589+GleammerRay@users.noreply.github.com>
2024-03-29 12:11:44 +0000

    Update payment_card_button.dart

--- lib/passy_flutter/widgets/payment_card_button.dart
+++ lib/passy_flutter/widgets/payment_card_button.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_credit_card/flutter_credit_card.dart';
-import 'package:websafe_svg/websafe_svg.dart';
-
+import 'package:flutter_svg/flutter_svg.dart';
 import '../../passy_data/payment_card.dart';
 import '../common/common.dart';
 
@@ -28,41 +27,49 @@ class PaymentCardButton extends StatelessWidget {
     return Center(
       child: Stack(
         children: [
-          TextButton(
-            onPressed: onPressed,
-            child: CreditCardWidget(
-              glassmorphismConfig: Glassmorphism.defaultConfig(),
-              width: 350,
-              height: 200,
-              cardNumber: ' ',
-              expiryDate: paymentCard.exp,
-              cardHolderName: paymentCard.cardholderName,
-              customCardTypeIcons: customCardTypeIcons ??
-                  [
-                    CustomCardTypeIcon(
-                        cardType: CardType.otherBrand,
-                        cardImage: WebsafeSvg.asset(
-                          'assets/images/logo_circle.svg',
-                          colorFilter: const ColorFilter.mode(
-                              Colors.purple, BlendMode.srcIn),
-                          width: 50,
-                        ))
-                  ],
-              cvvCode: '',
-              showBackView: false,
-              obscureCardNumber: obscureCardNumber,
-              obscureCardCvv: obscureCardCvv,
-              isHolderNameVisible: true,
-              isChipVisible: false,
-              backgroundImage: 'assets/images/payment_card_bg.png',
-              cardType: cardTypeFromNumber(
-                  paymentCard.cardNumber.replaceAll('*', '0')),
-              isSwipeGestureEnabled: isSwipeGestureEnabled,
-              onCreditCardWidgetChange: (brand) {},
+          InkWell(
+            borderRadius: const BorderRadius.all(Radius.circular(12)),
+            onTap: onPressed,
+            child: ClipRect(
+              child: Align(
+                heightFactor: 0.857,
+                widthFactor: 0.917,
+                child: CreditCardWidget(
+                  glassmorphismConfig: Glassmorphism.defaultConfig(),
+                  width: 350,
+                  height: 200,
+                  cardNumber: beautifyCardNumber(paymentCard.cardNumber),
+                  expiryDate: paymentCard.exp,
+                  cardHolderName: paymentCard.cardholderName,
+                  customCardTypeIcons: customCardTypeIcons ??
+                      [
+                        CustomCardTypeIcon(
+                            cardType: CardType.otherBrand,
+                            cardImage: SvgPicture.asset(
+                              'assets/images/logo_circle.svg',
+                              colorFilter: const ColorFilter.mode(
+                                  Colors.purple, BlendMode.srcIn),
+                              width: 50,
+                            ))
+                      ],
+                  cvvCode: '',
+                  showBackView: false,
+                  obscureCardNumber: obscureCardNumber,
+                  obscureCardCvv: obscureCardCvv,
+                  isHolderNameVisible: true,
+                  isChipVisible: false,
+                  backgroundImage: 'assets/images/payment_card_bg.png',
+                  cardType: cardTypeFromNumber(
+                      paymentCard.cardNumber.replaceAll('*', '0')),
+                  isSwipeGestureEnabled: isSwipeGestureEnabled,
+                  onCreditCardWidgetChange: (brand) {},
+                  bankName: ' ',
+                ),
+              ),
             ),
           ),
           Padding(
-            padding: const EdgeInsets.fromLTRB(35, 32, 0, 0),
+            padding: const EdgeInsets.fromLTRB(18, 12, 0, 0),
             child: Text(
               paymentCard.nickname,
               style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
