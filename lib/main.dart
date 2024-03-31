d09c6d1406a13c08591e7e41676600f34f55c381
GleammerRay <101527589+GleammerRay@users.noreply.github.com>
2024-03-31 00:50:56 +0000

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:passy_browser_extension/screens/note_screen.dart';
 import 'package:passy_browser_extension/screens/notes_screen.dart';
