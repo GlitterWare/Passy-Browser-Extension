a03040b982e066147c8e049b14aeb7aafcd8a06e
GleammerRay <101527589+GleammerRay@users.noreply.github.com>
2024-03-31 17:04:12 +0100

    Note.tags implemented

--- lib/passy_data/note.dart
+++ lib/passy_data/note.dart
@@ -1,5 +1,6 @@
 import 'common.dart';
 import 'entry_meta.dart';
+
 import 'passy_entries.dart';
 import 'passy_entry.dart';
 
@@ -7,17 +8,26 @@ typedef Notes = PassyEntries<Note>;
 
 class NoteMeta extends EntryMeta {
   final String title;
+  final List<String> tags;
 
-  NoteMeta({required String key, required this.title}) : super(key);
+  NoteMeta({
+    required String key,
+    required this.title,
+    required this.tags,
+  }) : super(key);
 
   NoteMeta.fromJson(Map<String, dynamic> json)
       : title = json['title'],
+        tags = json.containsKey('tags')
+            ? (json['tags'] as List<dynamic>).map((e) => e.toString()).toList()
+            : const [],
         super(json['key'] ?? '');
 
   @override
   toJson() => {
         'key': key,
         'title': title,
+        'tags': tags,
       };
 }
 
@@ -25,31 +35,54 @@ class Note extends PassyEntry<Note> {
   String title;
   String note;
   bool isMarkdown;
+  List<String> attachments;
+  List<String> tags;
 
   Note({
     String? key,
     this.title = '',
     this.note = '',
     this.isMarkdown = false,
-  }) : super(key ?? DateTime.now().toUtc().toIso8601String());
+    List<String>? attachments,
+    List<String>? tags,
+  })  : attachments = attachments ?? [],
+        tags = tags ?? [],
+        super(key ?? DateTime.now().toUtc().toIso8601String());
 
   @override
-  EntryMeta get metadata => NoteMeta(key: key, title: title);
+  EntryMeta get metadata => NoteMeta(
+        key: key,
+        title: title,
+        tags: tags,
+      );
 
   Note.fromJson(Map<String, dynamic> json)
       : title = json['title'] ?? '',
         note = json['note'] ?? '',
         isMarkdown = json['isMarkdown'] ?? false,
+        attachments = json['attachments'] == null
+            ? []
+            : (json['attachments'] as List<dynamic>)
+                .map((e) => e.toString())
+                .toList(),
+        tags = json['tags'] == null
+            ? []
+            : (json['tags'] as List<dynamic>).map((e) => e.toString()).toList(),
         super(json['key'] ?? DateTime.now().toUtc().toIso8601String());
 
   Note._fromCSV(List csv)
       : title = csv[1] ?? '',
         note = csv[2] ?? '',
         isMarkdown = boolFromString(csv[3] ?? 'false') ?? false,
+        tags = (csv[4] as List<dynamic>).map((e) => e.toString()).toList(),
+        attachments =
+            (csv[5] as List<dynamic>).map((e) => e.toString()).toList(),
         super(csv[0] ?? DateTime.now().toUtc().toIso8601String());
 
   factory Note.fromCSV(List csv) {
     if (csv.length == 3) csv.add('false');
+    if (csv.length == 4) csv.add([]);
+    if (csv.length == 5) csv.add([]);
     return Note._fromCSV(csv);
   }
 
@@ -62,6 +95,8 @@ class Note extends PassyEntry<Note> {
         'title': title,
         'note': note,
         'isMarkdown': isMarkdown,
+        'tags': tags,
+        'attachments': attachments,
       };
 
   @override
@@ -70,5 +105,7 @@ class Note extends PassyEntry<Note> {
         title,
         note,
         isMarkdown.toString(),
+        tags,
+        attachments,
       ];
 }
