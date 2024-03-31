bef6d0625661db5fe6d24991b3d501dd50b75245
GleammerRay <101527589+GleammerRay@users.noreply.github.com>
2024-03-31 20:40:52 +0100

    Update search_screen.dart

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -1,9 +1,13 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:passy_browser_extension/passy_browser_extension_flutter/widgets/browser_extension_appbar.dart';
 import '../common/common.dart';
 import '../passy_data/entry_type.dart';
 import '../passy_flutter/passy_theme.dart';
 import '../passy_flutter/widgets/widgets.dart';
+import 'common/common.dart';
 
 class SearchScreenArgs {
   String? title;
@@ -126,94 +130,105 @@ class _SearchScreen extends State<SearchScreen> {
       _load(args);
       _initialized = true;
     }
-    return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          padding: PassyTheme.appBarButtonPadding,
-          splashRadius: PassyTheme.appBarButtonSplashRadius,
-          icon: const Icon(Icons.arrow_back_ios_new_rounded),
-          onPressed: args.isAutofill
-              ? SystemNavigator.pop
-              : () => Navigator.pop(context),
-        ),
-        title: Text(args.title ?? localizations.search),
-        centerTitle: true,
-      ),
-      body: Column(
-        children: [
-          PassyPadding(TextFormField(
-              controller: queryController,
-              focusNode: queryFocus,
-              decoration: InputDecoration(
-                label: Text(localizations.search),
-                hintText: 'github human@example.com',
-              ),
-              onTap: () {
-                if (!queryFocus.hasFocus) {
-                  queryFocus.requestFocus();
-                  queryController.selection = TextSelection(
-                      baseOffset: 0,
-                      extentOffset: queryController.value.text.length);
-                }
-              },
-              onChanged: (s) {
-                setState(() {
-                  String lowerS = s.toLowerCase();
-                  notSelected.sort(
-                    (a, b) {
-                      a = a.toLowerCase();
-                      b = b.toLowerCase();
-                      int aMatches = lowerS.allMatches(a).length;
-                      int bMatches = lowerS.allMatches(b).length;
-                      if (aMatches == bMatches) return a.compareTo(b);
-                      return bMatches - aMatches;
-                    },
-                  );
-                  int baseOffset = queryController.selection.baseOffset;
-                  queryController.text = s;
-                  queryController.selection = TextSelection(
-                      baseOffset: baseOffset, extentOffset: baseOffset);
-                  entryBuilder ??= Future<void>.delayed(
-                      const Duration(milliseconds: 100), () {
-                    entryBuilder = null;
-                    setState(() {
-                      _widget =
-                          _builder(queryController.text, selected, rebuild);
-                    });
-                  });
-                });
-              })),
-          if (_loaded && (selected.isNotEmpty || notSelected.isNotEmpty))
-            Padding(
-                padding: EdgeInsets.only(
-                    top: PassyTheme.passyPadding.top / 2,
-                    bottom: PassyTheme.passyPadding.bottom / 2),
-                child: EntryTagList(
-                  notSelected: notSelected,
-                  selected: selected,
-                  onAdded: (tag) => setState(() {
-                    queryFocus.requestFocus();
-                    if (queryController.text.isNotEmpty) {
-                      queryController.text = '';
-                    }
-                    selected.add(tag);
-                    selected.sort();
-                    notSelected.remove(tag);
-                    _widget = _builder(queryController.text, selected, rebuild);
-                  }),
-                  onRemoved: (tag) => setState(() {
-                    queryFocus.requestFocus();
-                    selected.remove(tag);
-                    notSelected.add(tag);
-                    notSelected.sort();
-                    _widget = _builder(queryController.text, selected, rebuild);
-                  }),
-                )),
-          Expanded(
-            child: _widget,
+    return PopScope(
+        canPop: data.isEmbed ? false : true,
+        onPopInvoked: data.isEmbed ? (didPop) => logoutOnWillPop(this) : null,
+        child: Scaffold(
+          appBar: BrowserExtensionAppbar(
+            leading: IconButton(
+              padding: PassyTheme.appBarButtonPadding,
+              splashRadius: PassyTheme.appBarButtonSplashRadius,
+              icon: data.isEmbed
+                  ? Transform(
+                      alignment: Alignment.center,
+                      transform: Matrix4.rotationY(pi),
+                      child: const Icon(Icons.logout),
+                    )
+                  : const Icon(Icons.arrow_back_ios_new_rounded),
+              onPressed: data.isEmbed
+                  ? () => logOut(this)
+                  : () => Navigator.pop(context),
+            ),
+            title: Text(args.title ??
+                (data.isEmbed ? localizations.autofill : localizations.search)),
           ),
-        ],
-      ),
-    );
+          body: Column(
+            children: [
+              PassyPadding(TextFormField(
+                  controller: queryController,
+                  focusNode: queryFocus,
+                  decoration: InputDecoration(
+                    label: Text(localizations.search),
+                    hintText: 'github human@example.com',
+                  ),
+                  onTap: () {
+                    if (!queryFocus.hasFocus) {
+                      queryFocus.requestFocus();
+                      queryController.selection = TextSelection(
+                          baseOffset: 0,
+                          extentOffset: queryController.value.text.length);
+                    }
+                  },
+                  onChanged: (s) {
+                    setState(() {
+                      String lowerS = s.toLowerCase();
+                      notSelected.sort(
+                        (a, b) {
+                          a = a.toLowerCase();
+                          b = b.toLowerCase();
+                          int aMatches = lowerS.allMatches(a).length;
+                          int bMatches = lowerS.allMatches(b).length;
+                          if (aMatches == bMatches) return a.compareTo(b);
+                          return bMatches - aMatches;
+                        },
+                      );
+                      int baseOffset = queryController.selection.baseOffset;
+                      queryController.text = s;
+                      queryController.selection = TextSelection(
+                          baseOffset: baseOffset, extentOffset: baseOffset);
+                      entryBuilder ??= Future<void>.delayed(
+                          const Duration(milliseconds: 100), () {
+                        entryBuilder = null;
+                        setState(() {
+                          _widget =
+                              _builder(queryController.text, selected, rebuild);
+                        });
+                      });
+                    });
+                  })),
+              if (_loaded && (selected.isNotEmpty || notSelected.isNotEmpty))
+                Padding(
+                    padding: EdgeInsets.only(
+                        top: PassyTheme.passyPadding.top / 2,
+                        bottom: PassyTheme.passyPadding.bottom / 2),
+                    child: EntryTagList(
+                      notSelected: notSelected,
+                      selected: selected,
+                      onAdded: (tag) => setState(() {
+                        queryFocus.requestFocus();
+                        if (queryController.text.isNotEmpty) {
+                          queryController.text = '';
+                        }
+                        selected.add(tag);
+                        selected.sort();
+                        notSelected.remove(tag);
+                        _widget =
+                            _builder(queryController.text, selected, rebuild);
+                      }),
+                      onRemoved: (tag) => setState(() {
+                        queryFocus.requestFocus();
+                        selected.remove(tag);
+                        notSelected.add(tag);
+                        notSelected.sort();
+                        _widget =
+                            _builder(queryController.text, selected, rebuild);
+                      }),
+                    )),
+              Expanded(
+                child: _widget,
+              ),
+            ],
+          ),
+        ));
   }
 }
