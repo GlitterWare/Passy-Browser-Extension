55cd634df45a0c172648dd7b6527113f243b4919
GleammerRay <101527589+GleammerRay@users.noreply.github.com>
2024-04-04 15:49:21 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,9 +5,9 @@ git submodule update
 export PATH="$PATH:$PWD/submodules/flutter/bin"
 ./submodules/flutter/bin/flutter build web --web-renderer canvaskit --csp --pwa-strategy=none --dart-define=FLUTTER_WEB_CANVASKIT_URL=./canvaskit/ --wasm
 rm -rf build/firefox
-cp -r build/web build/firefox
+cp -r build/web_wasm build/firefox
 rm build/firefox/manifest.json
 mv build/firefox/manifest_firefox.json build/firefox/manifest.json
 echo 'All done.'
-echo "Chromium: $PWD/build/web"
+echo "Chromium: $PWD/build/web_wasm"
 echo "Firefox: $PWD/build/firefox"
